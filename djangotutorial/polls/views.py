0db3aa5dc7a0c033f18b6ad301a2ddd3997ec0a7
Axant Gondaliya <axantgondaliya0@gmail.com>
2026-02-19 00:05:02 +0530

    MVT, Error Handling and Forms (using forms.py)

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,22 +1,31 @@
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+
 from .models import Choice, Question
 
 
+# ---------- Function-based views ----------
+
 def index(request):
-    return HttpResponse("Hello, this is polls home page")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return HttpResponse(f"You are viewing question {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return HttpResponse(f"Results for question {question_id}")
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
@@ -25,11 +34,37 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        return HttpResponse("You didn't select a choice.")
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
-
         return HttpResponseRedirect(
             reverse("polls:results", args=(question.id,))
         )
+
+
+# ---------- Class-based views ----------
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
