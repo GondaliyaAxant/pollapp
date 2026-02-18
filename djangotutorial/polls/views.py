f119a9d46e88eded4307230711a6d09b03669c69
Axant Gondaliya <axantgondaliya0@gmail.com>
2026-02-18 23:43:34 +0530

    Django database and Admin Panel

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,25 +1,35 @@
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from .models import Question
+from django.urls import reverse
+from .models import Choice, Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, "polls/index.html", context)
+    return HttpResponse("Hello, this is polls home page")
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    return HttpResponse(f"You are viewing question {question_id}")
 
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+    return HttpResponse(f"Results for question {question_id}")
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return HttpResponse("You didn't select a choice.")
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(
+            reverse("polls:results", args=(question.id,))
+        )
